1bffca40808b3160908eeb238edab8c7985e852f
AlexNovikova <alexadubinina@mail.ru>
2023-11-17 12:26:15 +0700

    Добавьте файлы проекта.

---
+++ TPNumberLab3/TPNumber.h
@@ -0,0 +1,56 @@
+#ifndef MODERNPROGRAMMING_TPNUMBER_H
+#define MODERNPROGRAMMING_TPNUMBER_H
+
+#include <string>
+
+class TPNumber {
+public:
+	explicit TPNumber(double number = 0, int base = 10, int precision = 2);
+
+	explicit TPNumber(const std::string& number, const std::string& base = "10", const std::string& precision = "2");
+
+	TPNumber& operator=(const TPNumber& sec);
+
+	TPNumber operator+(const TPNumber& sec);
+
+	TPNumber operator-(const TPNumber& sec);
+
+	TPNumber operator*(const TPNumber& sec);
+
+	TPNumber operator/(const TPNumber& sec);
+
+	TPNumber sqr();
+
+	TPNumber inv();
+
+	TPNumber copy();
+
+	double getNumber();
+
+	std::string getNumberAsString();
+
+	int getBase();
+
+	std::string getBaseAsString();
+
+	void setBase(int base);
+
+	void setBase(const std::string& base);
+
+	int getPrecision();
+
+	std::string getPrecisionAsString();
+
+	void setPrecision(int precision);
+
+	void setPrecision(const std::string& precision);
+
+private:
+	double strtod(const std::string& num);
+
+	double _number;
+	int _base;
+	int _precision;
+};
+
+#endif // MODERNPROGRAMMING_TPNUMBER_H
